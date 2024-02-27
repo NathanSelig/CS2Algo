470504253cf08a66aec90062b2d49c27dc3b3561
Nathan Selig <nathanselig@gmail.com>
2024-02-27 02:43:19 -0500

    BUG: lines appearing to have 3 points this could just be a visual bug but it looks like some lines have 3 points in them my thought is that it is 2 points in the same place but this seems unlikes with the number of points and range that I input.

--- P3/src/App.java
+++ P3/src/App.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) {
 
-        StdDraw.setScale(0, 10);
+/*         StdDraw.setScale(0, 10);
         StdDraw.setPenRadius(0.01);
         StdDraw.setPenColor(StdDraw.BLACK);
         Point[] control = new Point[8];
@@ -16,9 +16,9 @@ public class App {
         control[4] = new Point(5, 7);
         control[5] = new Point(6, 8);
         control[6] = new Point(7, 9);
-        control[7] = new Point(8, 10);
+        control[7] = new Point(8, 10); */
         // BruteCollinearPoints bruteMethod = new BruteCollinearPoints(randomSetup());
-        FastCollinearPoints fastMethod = new FastCollinearPoints(control);
+        FastCollinearPoints fastMethod = new FastCollinearPoints(randomSetup());
 
         System.out.println(fastMethod.numberOfSegments());
         System.out.println(fastMethod.segments());
@@ -27,7 +27,7 @@ public class App {
 
     public static Point[] randomSetup() {
         Random rng = new Random();
-        int n = 100;
+        int n = 250;
         int size = 720;
         Point[] points = new Point[n];
         StdDraw.setCanvasSize(720, 720);
--- P3/src/FastCollinearPoints.java
+++ P3/src/FastCollinearPoints.java
@@ -1,21 +1,29 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.StdDraw;
+
 public class FastCollinearPoints {
     double[] slopes;
     ArrayList<LineSegment> lines;
 
     public FastCollinearPoints(Point[] points) {
         lines = new ArrayList<LineSegment>();
+
         // slopes is length minus 1 because origin can not make slope with itself
         // starting at 1 because 0 is origin
         for (int i = 0; i < points.length; i++) {
+            Arrays.sort(points, i, points.length);
             Point origin = points[i];
-            slopes = new double[points.length - i - 1];
             Arrays.sort(points, i, points.length, origin.slopeOrder());
+            slopes = new double[points.length - i - 1];
+
+            // !need to debug this for the slopes because slopes of unwanted points
+            // *decided to change to making an array with a new size instead
+            // being calculated and moved
             for (int j = i + 1; j < points.length; j++) {
                 double slope = origin.slopeTo(points[j]);
-                slopes[j - i - 1] = slope;
+                slopes[j - 1 - i] = slope;
             }
             int k = 0;
             int numOfSlopes = 0;
@@ -23,8 +31,17 @@ public class FastCollinearPoints {
                 if (slopes[k] == slopes[k + 1]) {
                     numOfSlopes++;
                 }
-                if (numOfSlopes >= 2 && (slopes[k] != slopes[k + 1] || k == slopes.length - 2)) {
-                    LineSegment line = new LineSegment(origin, points[k + 1 + i]);
+                if (numOfSlopes >= 2 && slopes[k] != slopes[k + 1]) {
+                    LineSegment line = new LineSegment(origin, points[k + i + 1]);
+                    StdDraw.setPenRadius(0.001);
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                    line.draw();
+                    lines.add(line);
+                    numOfSlopes = 0;
+                } else if (numOfSlopes >= 2 && k == slopes.length - 2) {
+                    LineSegment line = new LineSegment(origin, points[k + i + 2]);
+                    StdDraw.setPenRadius(0.001);
+                    StdDraw.setPenColor(StdDraw.BLACK);
                     line.draw();
                     lines.add(line);
                     numOfSlopes = 0;
